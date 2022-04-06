2237084f56ee59d9f0e319b99355fc36d86ef6a6
Daniele Segreto <segreto.daniele1989@libero.it>
2022-04-06 15:58:36 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import { useState, useEffect } from "react";
 import data from "./data";
-import Articolo from "./Articolo";
+import Articolo from "./Articolo"; // Componente
 
-//Funzione che se presente 'Theme' nel localStorage
+// Funzione che se presente 'Theme' nel localStorage
 // returna il suo valore o di default return 'light-mode'
 
 function App() {
   return (
     <section className="section-center">
       <div className="container">
-        <h2>DARK MODE APP</h2>
+        <button className="btn">
+          Cambia
+        </button>
+
+        {/* Avendo importato il file data.js, ho passato con lo Spread Operator, ogni singolo elelemto di questo array di oggetti, all'interno del nostro componente Articolo */}
+        <section className="article-section">
+          {
+            data.map(el => <Articolo key={el.id} {...el} />)
+          }
+        </section>
       </div>
     </section>
   );
 }
 
 export default App;
+
+{/* A JSX comment */}
--- src/Articolo.js
+++ src/Articolo.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Articolo = () => {
+// Decostruzione dell'oggetto | Prendo gli elementi che mi servono da data.js
+const Articolo = ({ body, title }) => {
   return (
     <article>
-      <h6>Articolo</h6>
+      <div style={{ position: "relative", height: "fit-content" }}>
+        <h4> {title} </h4>
+        <div className="underline"></div>
+      </div>
+      <p> {body}</p>
     </article>
   );
 };
